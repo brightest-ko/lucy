37825ff9bd0c473bc102847d04feabc305ee925f
lucky-hee <gobaksa4@naver.com>
2018-03-12 18:32:05 +0900

    bootstrap가능

--- sns/app/assets/javascripts/application.js
+++ sns/app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
-//= require turbolinks
+//= require materialize-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+    // Handler for .ready() called.
+  });
